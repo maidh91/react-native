5f178b3e6cbbd84b614b47f94ed401148a4c457e
maidinh <maidinh@postech.ac.kr>
2017-12-02 09:11:54 +0900

    init one-time-password

---
+++ one-time-password/functions/create_user.js
@@ -0,0 +1,21 @@
+const admin = require('firebase-admin');
+
+module.exports = (req, res) => {
+  // test
+  // res.send(req.body);
+
+  // verify the user provided a phone
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });
+  }
+
+  // format the phone number to remove dashes and parens
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+  // create a new user acc using that phone number
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(error => res.status(422).send({ error }));
+
+  // response to the user request, saying the account was made
+}
---
+++ one-time-password/functions/twilio.js
@@ -0,0 +1,5 @@
+const twilio = require('twilio');
+
+const accountSid = 'ACed2783f5d2e1f0df6b500ad380c3e5eb';
+const authToken = '2d4b413ecf116e2861e2ac0f88ed2601';
+
