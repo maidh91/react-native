870d538f4ff2e2b262cac4778a02d79afe6652ca
maidinh <maidinh@postech.ac.kr>
2017-12-09 10:16:51 +0900

    one-time-password ok

---
+++ one-time-password/functions/verify_one_time_password.js
@@ -0,0 +1,32 @@
+const admin = require('firebase-admin');
+
+module.exports = (req, res) => {
+  if (!req.body.phone || !req.body.code) {
+    return res.status(422).send({ error: 'Phone and code must be provided' });
+  }
+
+  var prefix = '';
+  if (String(req.body.phone).startsWith('+')) {
+    prefix = '+';
+  }
+  const phone = prefix + String(req.body.phone).replace(/[^\d]/g, "");
+  const code = parseInt(req.body.code);
+
+  admin.auth().getUser(phone)
+    .then(() => {
+      const ref = admin.database().ref('users/' + phone)
+      ref.on('value', snapshot => {
+        ref.off();      // stop listening to value changes
+        const user = snapshot.val();
+
+        if (user.code !== code || !user.codeValid) {
+          return res.status(422).send({ error: 'Code not valid' });
+        }
+
+        ref.update({ codeValid: false })
+        admin.auth().createCustomToken(phone)
+          .then(token => res.send(token))
+      });
+    })
+    .catch(error => res.status(422).send(error))
+}
